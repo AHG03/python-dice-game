6c964c0459223ef0c10f911cf28ef5a2adb8976a
Ali Ahangar <aliahangar80@gmail.com>
2025-10-20 15:59:20 +0330

    change variable to be more descrptive. Added while loop for continuous play.

--- dice.py
+++ dice.py
@@ -1,13 +1,13 @@
 import random
 
-message = input("Roll the dice? (y/n): ").lower().strip()
+choice = input("Roll the dice? (y/n): ").lower().strip()
 
-if message == "y":
-    x = random.randint(1,6)
-    y = random.randint(1,6)
-    print(f"({x}, {y})")
-elif message == "n":
-    print("Thanks for playing!")
-
-else:
-    print("invalid choice!")
+while True:
+    if choice == "y":
+        die1 = random.randint(1,6)
+        die2 = random.randint(1,6)
+        print(f"({die1}, {die2})")
+    elif choice == "n":
+        print("Thanks for playing!")
+    else:
+        print("invalid choice!")
